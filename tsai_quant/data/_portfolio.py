c5c07d5a52344fe79168e07d47283bf13940e6b9
Dominique <dominique@garmier.ch>
2021-04-24 17:00:51 +0200

    added more files

--- tsai_quant/data/_portfolio.py
+++ tsai_quant/data/_portfolio.py
@@ -1,2 +1,12 @@
+'''
+--------
+Copyright (c) 2021 tildeSlashAi, Dominique Garmier
+
+All rights reserved.
+--------
+
+Portfolio is a Class describing an list of Assets
+
+'''
 class Portfolio:
     pass
--- tsai_quant/data/_prices.py
+++ tsai_quant/data/_prices.py
@@ -1,2 +1,13 @@
+'''
+--------
+Copyright (c) 2021 tildeSlashAi, Dominique Garmier
+
+All rights reserved.
+--------
+
+Prices is a base class used in DailyPrices
+
+'''
+
 class Prices:
     pass
